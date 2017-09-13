7e0e5f702695bff5dcd348b75477ecafdd3fd2a8
Lachlan Donald <lachlan@ljd.cc>
2017-09-14 08:28:37 +1000

    Split args out into own file

---
+++ args_test.go
@@ -0,0 +1,76 @@
+package bintest_test
+
+import (
+	"testing"
+
+	"github.com/lox/bintest"
+)
+
+func TestArgumentsThatDontMatch(t *testing.T) {
+	var testCases = []struct {
+		expected bintest.Arguments
+		actual   []string
+	}{
+		{
+			bintest.Arguments{"test", "llamas", "rock"},
+			[]string{"test", "llamas", "alpacas"},
+		},
+		{
+			bintest.Arguments{"test", "llamas"},
+			[]string{"test", "llamas", "alpacas"},
+		},
+	}
+
+	for _, test := range testCases {
+		match, _ := test.expected.Match(test.actual...)
+		if match {
+			t.Fatalf("Expected %v and %v to not match", test.expected, test.actual)
+		}
+	}
+}
+
+func TestArgumentsThatMatch(t *testing.T) {
+	var testCases = []struct {
+		expected bintest.Arguments
+		actual   []string
+	}{
+		{
+			bintest.Arguments{"test", "llamas", "rock"},
+			[]string{"test", "llamas", "rock"},
+		},
+		{
+			bintest.Arguments{"test", "llamas", bintest.MatchAny()},
+			[]string{"test", "llamas", "rock"},
+		},
+	}
+
+	for _, test := range testCases {
+		match, _ := test.expected.Match(test.actual...)
+		if !match {
+			t.Fatalf("Expected %v and %v to match", test.expected, test.actual)
+		}
+	}
+}
+
+func TestArgumentsToString(t *testing.T) {
+	var testCases = []struct {
+		args     bintest.Arguments
+		expected string
+	}{
+		{
+			bintest.Arguments{"test", "llamas", "rock"},
+			`"test" "llamas" "rock"`,
+		},
+		{
+			bintest.Arguments{"test", "llamas", bintest.MatchAny()},
+			`"test" "llamas" *`,
+		},
+	}
+
+	for _, test := range testCases {
+		actual := test.args.String()
+		if actual != test.expected {
+			t.Fatalf("Expected [%s], got [%s]", test.expected, actual)
+		}
+	}
+}
