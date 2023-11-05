aa498641cce97d8d1089470ff722729749c54dee
Josh Deprez <jd@buildkite.com>
2023-11-06 10:23:30 +1100

    Add test that detects write-exec race

--- compiler_test.go
+++ compiler_test.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strconv"
+	"testing"
 
 	"github.com/buildkite/bintest/v3"
 )
@@ -42,4 +44,40 @@ func ExampleCompileProxy() {
 	}
 
 	// Output: Llama party! 🎉
+}
+
+func TestCompileProxy_GoBug22315(t *testing.T) {
+	// On Linux (and possibly other Unices), there exists a race condition that
+	// manifests when you write and then execute a binary file in a multi-
+	// -threaded program. See https://github.com/golang/go/issues/22315.
+	// This is debatably a bug in either Linux or Go, but the Go bug has a
+	// lengthy discussion.
+	//
+	// bintest used to operate by copying the compiled binaries around. Copying
+	// necessarily requires opening the destination file for writing. Then,
+	// bintest provides the path of the freshly-copied binary to the test that
+	// is using bintest, which usually immediately executes that binary.
+	// Throw in a bit of t.Parallel and you have a recipe for test flakes.
+
+	t.Parallel()
+	for i := 0; i < 1000; i++ {
+		name := fmt.Sprintf("%s-%d", t.Name(), i)
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			t.Parallel()
+
+			m, err := bintest.NewMock(name)
+			if err != nil {
+				t.Fatalf("bintest.NewMock(%q) error = %v", name, err)
+			}
+			defer m.CheckAndClose(t)
+
+			m.Expect().AndExitWith(0)
+
+			// If file copying is used in process, and the race conditions are
+			// unfavourable, Run will fail with "text file busy" (ETXTBSY).
+			if err := exec.Command(m.Path).Run(); err != nil {
+				t.Errorf("exec.Command(%q).Run() = %v", m.Path, err)
+			}
+		})
+	}
 }
